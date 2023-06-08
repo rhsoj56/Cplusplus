463eb2f0c142d6223317ff6fee66387071d2e244
Josh Rowell <rowelljosh6@gmail.com>
2023-06-08 21:31:49 +0100

    first cpp script

---
+++ temp_converter.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  
+  double tempf = 69;
+  double tempc;
+
+  tempc = (tempf - 32) / 1.8;
+
+  std::cout << "The temp is " << tempc << " degrees Celsius.\n";
+   
+}
